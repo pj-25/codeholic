37583f4b7eda563b9a3c8a4195d8b9c9f0367293
Joshi Prashant <53967712+pj-25@users.noreply.github.com>
2021-03-05 18:20:55 +0530

    updating downloader

--- chitChatApp/chitChatDownloader/DownloadChitChat.java
+++ chitChatApp/chitChatDownloader/DownloadChitChat.java
@@ -1,6 +1,5 @@
-package chitChatDownloader;
+package chitChat.chitChatDownloader;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,6 +14,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.ProgressBar;
@@ -24,14 +26,33 @@ public class DownloadChitChat extends Application {
     private ProgressBar progressBar = new ProgressBar();
     private Label status;
     private final String DOWNLOAD_LINK = "https://github.com/pj-25/codeholic/raw/main/chitChatAppV2.jar";
+    private Button downloadBtn;
+
+    private boolean isPaused = false;
+    private final Object pauseLock = new Object();
 
     @Override
     public void start(Stage primaryStage){
-        Button downloadBtn = new Button("Download");
+        downloadBtn = new Button("Download");
         downloadBtn.setOnAction(e->{
             try{
-                download();
-                downloadBtn.setDisable(true);
+                String btnStatus = downloadBtn.getText();
+                if(btnStatus.equals("Download") || btnStatus.equals("Resume")){
+                    if(!isPaused){
+                        download();
+                    }
+                    else{
+                        resume();
+                    }
+                    downloadBtn.setText("Pause");
+                }
+                else if(btnStatus.equals("Pause")){
+                    downloadBtn.setText("Resume");
+                    isPaused = true;
+                }
+                else{
+                    launchChitChat();
+                }
             }
             catch (IOException x){
                 status.setText("Unable to connect with Server :(");
@@ -39,6 +60,10 @@ public class DownloadChitChat extends Application {
         });
         Label fileType = new Label("Linux/Ubuntu (15.3MB)");
         status = new Label("Click above to download");
+        fileType.setFont(Font.font("Monospace", FontWeight.EXTRA_BOLD, 20));
+        fileType.setTextFill(Color.GREEN);
+
+        progressBar.setMinWidth(400);
 
         VBox vbox = new VBox(progressBar, downloadBtn, fileType, status);
         vbox.setSpacing(10);
@@ -50,7 +75,7 @@ public class DownloadChitChat extends Application {
         spane.getStyleClass().add("login-background");
 
         Scene mainScene = new Scene(spane, 600, 400);
-        mainScene.getStylesheets().add("chitChatApp/style.css");
+        mainScene.getStylesheets().add("chitChat/chitChatApp/style.css");
 
         primaryStage.setScene(mainScene);
         primaryStage.setTitle("ChitChat");
@@ -75,6 +100,22 @@ public class DownloadChitChat extends Application {
         }
     }
 
+    public void resume(){
+        synchronized (pauseLock){
+            isPaused = false;
+            pauseLock.notifyAll();
+        }
+    }
+
+    public void launchChitChat() {
+        try{
+            Process runChitChat = Runtime.getRuntime().exec("java -jar chitChatApp.jar");
+        }
+        catch (IOException e){
+            status.setText("Unable to launch ChitChat :(");
+        }
+    }
+
     public void download() throws IOException{
         progressBar.setProgress(0);
 
@@ -85,29 +126,34 @@ public class DownloadChitChat extends Application {
         System.out.println(TOTAL_SIZE);
 
         InputStream inStream = urlConnection.getInputStream();
-        FileOutputStream fout = new FileOutputStream(new File("chitChatApp.jar"));
+        FileOutputStream fout = new FileOutputStream("chitChatApp.jar");
 
         status.setText("Download in progress...");
 
         new Thread(()->{
             try{
+                isPaused = false;
                 float dsize = 0;
                 byte []data = new byte[1024];
                 int fp;
                 while((fp=inStream.read(data, 0,1024))>=0){
+                    synchronized (pauseLock){
+                        while(isPaused){
+                            pauseLock.wait();
+                        }
+                    }
                     fout.write(data, 0, fp);
                     dsize += fp;
                     progressBar.setProgress(dsize / (TOTAL_SIZE));
                 }
-                System.out.println(progressBar.getProgress());
-                System.out.println(dsize);
                 inStream.close();
                 fout.close();
-                Platform.runLater(()->{
+                Platform.runLater(()-> {
+                    downloadBtn.setText("Launch");
                     status.setText("Downloaded successfully!");
                 });
             }
-            catch (IOException x){
+            catch (IOException | InterruptedException x){
                 status.setText("Error in download :(");
             }
         }).start();
